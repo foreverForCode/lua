9065727dcab6f0022bc02dad2fac94b1e4840142
jorden <www.freewm@163.com>
2017-12-28 15:42:08 +0800

    del ~.file

---
+++ lua/test/realdata.lua
@@ -0,0 +1,18 @@
+local template = require "resty.template"
+local _M = {}
+
+
+
+
+
+function _M.index()
+  
+  template.render("real.html",model)
+  return "";
+
+end
+
+
+local getHtmlContent = _M.index()
+
+ngx.say(getHtmlContent)
